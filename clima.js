aa63c97ab3cba91dc74977b63a8dcdfbb908ef2f
Duda Jardim <maria.sousa437@etec.sp.gov.br>
2025-10-10 14:26:30 -0300

    atualizado 10/10/2025

--- clima.js
+++ clima.js
@@ -3,11 +3,14 @@ document.querySelector('.busca').addEventListener('submit',(event)=>{
 
     let input = document.querySelector('#searchInput').value
 
-    if(input !== ''){
+    if(input !== ""){
         showWarning("Carregando...")
 
-        let results = await fetch('https://api.openweathermap.org/data/2.5/weather?q=${encodeURL(input)}&appid=62f1cfa0e9f235e549032d2752765265&units=metrics&lang=pt_br');
-        let json = await results.json();
+        let result = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=
+        ${ encodeURL(input) }&appid=6e970309ab369df73f38c10918431f77&units=metrics&lang=pt_br`)
+        let json =  await results.json();
+
+        console.log(json)
 
         if (json.cod == 200) {
             showInfo(({
